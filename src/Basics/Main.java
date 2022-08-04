e9b481528e05585ebb7413b99c0a0f1c7321b0f8
shivamface2face <shivamfaceuit@gmail.com>
2022-08-04 21:21:08 +0530

    class basic

---
+++ src/Basics/Main.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class Main {
+
+    class Student{
+        String name;
+        int roll;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        Student shivam;
+
+    }
+}
